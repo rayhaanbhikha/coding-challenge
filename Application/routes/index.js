e066b2c75eaf24176aa537de4381c7337eb29430
Ray <rayhaan.bhikha@sky.uk>
2019-05-15 00:22:41 +0100

    Moved all code into application directory.

--- Application/routes/index.js
+++ Application/routes/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 
 const { home, users } = require("../controllers");
+const { checkUserId, checkUserStreams } = require("../middleware");
 
 app.get("/", home);
 app.get("/api/users", users.allUsers);
 app.get("/api/users/:userId/streams", users.getStreamsById);
+
+app.get("/api/video", checkUserId, checkUserStreams);
 
 module.exports = app;
