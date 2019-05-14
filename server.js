e49df5de16ac2b53c5ebed588d581eddf6998ab7
Ray <rayhaan.bhikha@sky.uk>
2019-05-14 22:42:38 +0100

    Create home endpoint and extract routes into seperate file.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-
+const routes = require("./routes");
 const port = process.env.PORT || 8080;
+
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`server started on port ${port}`);
