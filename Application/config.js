818026e7ae31b3c2032457c169641a0317b5d78f
Ray <rayhaan.bhikha@sky.uk>
2019-05-15 16:21:38 +0100

    config header concurrentlimit now dependant on env

--- Application/config.js
+++ Application/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    userIdHeader: "user-id",
-    concurrentStreamLimit: 3,
+    userIdHeader: process.env.USER_ID_HEADER || "user-id",
+    concurrentStreamLimit: parseInt(process.env.CONCURRENT_STREAM_LIMIT, 10) || 3,
     dbHOST: process.env.DB_HOST || "localhost",
     dbPORT: parseInt(process.env.DB_PORT, 10) || 3000,
     port: parseInt(process.env.PORT, 10) || 8080
