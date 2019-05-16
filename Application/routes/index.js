02408031d4d5a33dacb6b1fc9b744c0e1132be84
Ray <rayhaan.bhikha@sky.uk>
2019-05-16 11:46:10 +0100

    Swagger docs added to api server.

--- Application/routes/index.js
+++ Application/routes/index.js
@@ -3,8 +3,12 @@ const app = express();
 
 const { home, users, video } = require("../controllers");
 const { checkUserId, checkUserStreams } = require("../middleware");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require("../swagger");
 
 app.get("/", home);
+app.use("/docs", swaggerUi.serve)
+app.get("/docs", swaggerUi.setup(swaggerDoc));
 app.get("/api/users", users.allUsers);
 app.get("/api/users/:userId/streams", users.getStreamsById);
 
