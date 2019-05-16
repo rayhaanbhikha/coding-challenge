291308647e740a32fbcae41e912d7bd9fb936e2e
Ray <rayhaan.bhikha@sky.uk>
2019-05-16 11:35:53 +0100

    changed userId header in config.

--- Application/config.js
+++ Application/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    userIdHeader: process.env.USER_ID_HEADER || "user-id",
+    userIdHeader: process.env.USER_ID_HEADER || "x-user-id",
     concurrentStreamLimit: parseInt(process.env.CONCURRENT_STREAM_LIMIT, 10) || 3,
     dbHOST: process.env.DB_HOST || "localhost",
     dbPORT: parseInt(process.env.DB_PORT, 10) || 3000,
