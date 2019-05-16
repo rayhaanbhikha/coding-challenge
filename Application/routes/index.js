45a5c113f8d7ac93af316cf82bb821578ce19a12
Ray <rayhaan.bhikha@sky.uk>
2019-05-16 10:59:00 +0100

    Added route to handle calls to non existent api endpoint.

--- Application/routes/index.js
+++ Application/routes/index.js
@@ -10,4 +10,5 @@ app.get("/api/users/:userId/streams", users.getStreamsById);
 
 app.get("/api/video", checkUserId, checkUserStreams, video);
 
+app.all("*", (req, res) => res.status(404).send("Not found"));
 module.exports = app;
